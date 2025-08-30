59c769aea0a45da4da1bede68fe91ba0d1bd8d09
Govind <141383976+govin07@users.noreply.github.com>
2025-08-30 19:17:41 +0530

    day-02 python

--- hello.py
+++ hello.py
@@ -1,1 +1,42 @@
 print('hello')
+
+str = ['A','B']
+obj = {'A':"1", 'B': "2"}
+
+set = {1.2,2.5,50.5}
+
+
+def sayHi(name):
+    print('heloo',name)
+
+def add(a ,b):
+    print(a + b)
+
+add(1 ,2)
+
+def sub(a,b):
+    print(a - b)
+
+sub(20,15)
+
+def mul(a,b):
+    print(a*b)
+
+mul(2,5)
+
+def dev(a,b):
+    print(a%b)
+
+def working(n):
+    print('python working perfect',n)
+
+dev(6,8)
+
+
+
+
+print(type([1,2,3000]))
+print(str,type(str))
+print(obj, type(obj))
+print(set,type(set))
+working("bro")
