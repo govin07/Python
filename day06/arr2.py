03bd154f0924e376e42e50de2a6e7b56ed85f589
Govind <141383976+govin07@users.noreply.github.com>
2025-09-30 03:06:57 +0530

    loop

---
+++ day06/arr2.py
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5,6,7,8,9]
+
+for index,value in enumerate(arr):
+    if value % 2 == 0 :
+       print(f"index is {index} of even number")
+    else:
+        print(f"value of  odd {value}")   
