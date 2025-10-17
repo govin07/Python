8136e16294648a0693cd1a8ebfd0246af8aa6742
Govind <141383976+govin07@users.noreply.github.com>
2025-10-17 20:30:26 +0530

    patterns

---
+++ day08/print.py
@@ -0,0 +1,1 @@
+print("india","pakistan","shrilanka","nepla",sep="-")
--- day09/star.py
+++ day09/star.py
@@ -1,2 +1,14 @@
-for i in range(1,11):
-    print("*"*i,end="\n")
+# row = 6 
+# for i in range(1,row + 1):
+#     print('',end=" ")
+#     for j in range(0,i):
+#         print("*",end=" ")
+#     print('')
+
+
+c = "country"
+    
+    
+a = c.capitalize()
+b = c.title()
+print(a,b)
