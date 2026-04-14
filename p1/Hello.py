4a20c0af14248790e8c2daac547438bbe563af19
Govind <141383976+govin07@users.noreply.github.com>
2026-04-15 00:22:38 +0530

    learning python day 1

---
+++ p1/Hello.py
@@ -0,0 +1,48 @@
+print("Govind", 22 , "indore" ,sep="\n")
+
+print("Hello, World!")
+print("I am learning Python.")
+print("It is fun!")
+
+
+print("**********")
+print("*  Hello *", sep='\n')
+print("**********")
+
+name = "Govind Singh Solanki"
+age = 22
+city = 'indore'
+print("My name is ", name , end='')
+print("I am ",age,'years old', end='')
+print("i live in", city) 
+
+
+#  swap  tow variables 
+
+a = 5
+b = 10 
+print("before swap a",a)
+print("before swap b",b)
+a=a+b
+b=a-b
+a = a - b
+print("after swap a",a)
+print("after swap b",b)
+
+
+num = 20
+num2 = 3.14
+name1 = "God"
+isTrue = True
+print(num, type(num))
+print(num2, type(num2))
+print(name1,type(name1))
+print(isTrue,type(isTrue))
+
+Student = True
+print("=====ID Card=====")
+print("Name : ", name)
+print("Age  : ",age)
+print("City : " ,city)
+print("Studend : ", Student)
+print("================")
