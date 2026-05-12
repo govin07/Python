0b1a04b276dcdbc899bb31425fbb326af188610a
Govind <141383976+govin07@users.noreply.github.com>
2026-05-12 23:52:05 +0530

    numpy

---
+++ numpy/intro.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+print(np.array([1,2,3]))
+
+b = np.array([1,2,30],dtype=bool)
+
+print(b)
+
+ran = np.arange(1,44)
+
+# print(ran.reshape(5,2))
+
+one = np.ones((3,4))
+print(one)
+
+lin = np.linspace(-10,10,10)
+print(lin)
+print("hello")
